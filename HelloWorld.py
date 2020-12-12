a9194ac07e4d31b949d81647a435bf84f06bff89
Daniel-Loaiza <danielloaizaisi@gmail.com>
2020-12-12 10:22:54 -0500

    Definiton of the greet() function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,14 @@
+def greet(who="World"):
+    """Prints a greeting to the user
+
+    >>> greet(Pedro)
+    Hello Pedro!
+    """
+    print ("Hello "+who+"!")
+
 if __name__=="__main__":
-    print("Hello world!")
+    greet() #Calls the function to print the default option
+    greet(who='amigo') #Calls the function to print with a specific argument 
+    name=input("Hey, What's your name?\n")
+    greet(name) #Calls the function to print with a specific argument
+    help(greet) #Prints the Docstrings of the function
