d64461ce3f7919e144401b2a0a674b950fbf0016
Daniel-Loaiza <danielloaizaisi@gmail.com>
2020-12-13 11:24:24 -0500

    HelloWorld File improved

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,6 +9,14 @@ def greet(who="World"):
 if __name__=="__main__":
     greet() #Calls the function to print the default option
     greet(who='amigo') #Calls the function to print with a specific argument 
-    name=input("Hey, What's your name?\n")
-    greet(name) #Calls the function to print with a specific argument
-    help(greet) #Prints the Docstrings of the function
+    name=''
+    while name != 'q!':
+        name=input("Hey, What's your name?\n")
+        if name=='q!':
+            print("Exiting program...")
+        elif name=='':
+            greet()
+        else:
+            greet(name) #Calls the function to print with a specific argument
+    #help(greet) #Prints the Docstrings of the function
+
